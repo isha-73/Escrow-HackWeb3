d804c7d899a8262265a61955b0d73c86a650f5c4
Tilak Dave <davetilak003@gmail.com>
2023-07-16 01:04:53 +0530

    prettier format

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Testimonials from "@/components/Testimonials";
+import Testimonials from '@/components/Testimonials';
 
 export default function Home() {
   return (
@@ -6,12 +6,11 @@ export default function Home() {
       <div className="bg-dark shadow-md rounded-lg p-6  text-center  w-full sm:w-2/3 lg:w-1/2">
         <h2 className="text-2xl font-bold mb-4">Product Description</h2>
         <p className="text-gray-500">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam mauris
-          vel lectus vulputate tempus. Duis id arcu elit. Aenean malesuada sem ut
-          libero bibendum, et feugiat risus condimentum. Proin auctor mauris ac felis
-
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam
+          mauris vel lectus vulputate tempus. Duis id arcu elit. Aenean
+          malesuada sem ut libero bibendum, et feugiat risus condimentum. Proin
+          auctor mauris ac felis
         </p>
-
       </div>
       <div className="p-10">
         <Testimonials />
