fb9517efcd46cde9cf7d19190239fb9a6209425a
Isha Bule <ishabule753@gmail.com>
2023-07-21 01:15:35 +0530

    web3_hooks removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,10 @@
 import '../styles/globals.css';
-// import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
-
-// import 'regenerator-runtime/runtime';
 
 export default function App({ Component, pageProps }) {
-  // const supportedChainIds = [80001, 4];
 
-  // const connectors = {
-  //   injected: {}
-  // };
   return (
     <>
-      {/* <ThirdwebWeb3Provider
-        supportedChainIds={supportedChainIds}
-        connectors={connectors}
-      > */}
       <Component {...pageProps} />
-      {/* </ThirdwebWeb3Provider> */}
     </>
   );
 }
