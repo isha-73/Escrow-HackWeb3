23c732c777481e3bb84ec7d05ceaa9b8b80d27f6
Isha Bule <ishabule753@gmail.com>
2023-07-20 21:25:45 +0530

    metamask connection

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,24 @@
 import Link from 'next/link';
-const Navbar = () => {
+import { useRouter } from 'next/router';
+import { useWeb3 } from "@3rdweb/hooks";
+
+const Navbar = (props) => {
+  const { connectWallet, address, error } = useWeb3();
+
+  const router = useRouter();
+
+  const handleConnect = async () => {
+    try {
+      await connectWallet("injected");
+      console.log(address);
+      // Assuming successful connection, navigate to the /dashboard page
+      router.push('/dashboard');
+    } catch (error) {
+      console.error('Error connecting with MetaMask:', error);
+    }
+  };
   return (
-    <nav className="fixed top-0 left-0 w-full flex sm:flex-wrap justify-center backdrop-blur-sm bg-gray-700 bg-opacity-50 border border-blue-500 border-t-0 border-l-0  shadow-neon-blue">
+    <nav className="fixed top-0 left-0 w-full flex sm:flex-wrap justify-center backdrop-blur-sm bg-gray-700 bg-opacity-50 border border-blue-500 border-t-0 border-l-0 border-r-0  shadow-neon-blue ">
       <div className="container flex items-center justify-between p-2 ">
         <div>
           {/* <Image src="/logo.png" width={100} height={100} alt="logo" /> */}
@@ -11,10 +28,13 @@ const Navbar = () => {
         </div>
         <div className="space-x-4">
           <button
-            href="/about"
-            className="text-white hover:text-gray-300 bg-gradient-to-r from-purple-500 to-blue-500 bg-origin-border py-2 px-4 rounded-md neon-text"
+            className={`text-white hover:text-gray-300 bg-gradient-to-r from-purple-500 to-blue-500 bg-origin-border py-2 px-4 rounded-md neon-text ${props.disabled ? 'group-disabled:opacity-10 bg-gradient-to-r from-purple-300 to-blue-300 text-gray-300 group-disabled:pointer-events-none' : 'group'
+              }`}
+            onClick={handleConnect}
+            disabled={props.disabled}
           >
-            Connect
+            {/* {!props.disabled ? 'Connect Wallet' : 'Connected'} */}
+            Connect Wallet {console.log(address)}
           </button>
         </div>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
 import '../styles/globals.css';
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
+import "regenerator-runtime/runtime";
+
+
 
 export default function App({ Component, pageProps }) {
+  const supportedChainIds = [80001, 4];
+
+  const connectors = {
+    injected: {},
+  };
   return (
     <>
-      <Component {...pageProps} />
+      <ThirdwebWeb3Provider
+        supportedChainIds={supportedChainIds}
+        connectors={connectors}
+      >
+        <Component {...pageProps} />
+      </ThirdwebWeb3Provider>
     </>
   );
 }
