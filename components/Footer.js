ed825f3a4f9911f035ec595df47823be1e99a96d
Isha Bule <ishabule753@gmail.com>
2023-07-25 01:42:03 +0530

    eth request err fixed

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <div className=" inset-x-0 bottom-0 h-0.5 bg-gradient-to-r from-purple-500 to-blue-500"></div>
+      <div className=" inset-x-0  h-0.5 bg-gradient-to-r from-purple-500 to-blue-500"></div>
       <footer className=" text-white text-center py-4  bg-gray-700 bg-opacity-30 border border-gray-500 border-t-0 border-l-0">
         <div className="container mx-auto relative">
           <p>&copy; {new Date().getFullYear()} Made by ISNTI</p>
--- components/Home.js
+++ components/Home.js
@@ -24,7 +24,7 @@ export default function Home() {
           <Testimonials />
         </div>
       </main>
-      <footer className="my-0">
+      <footer>
         <Footer />
       </footer>
     </>
